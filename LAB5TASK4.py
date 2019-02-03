95d5886762f6ce28468d23e04f87734079fe7081
= <drahtantnayak@dal.ca>
2019-02-03 15:58:24 -0400

    Create class for time objects

---
+++ LAB5TASK4.py
@@ -0,0 +1,16 @@
+from time import gmtime, strftime
+
+# print("Current Time is:",strftime("%H:%M:%S", gmtime()))
+
+class Time():
+    def __init__(self,t1,t2):
+         self.t1 = t1
+         self.t2 = t2
+
+    def __ne__(self)
+         return self.t1 < self.t2
+
+def is_after():
+    flag = False
+    
+    return
