50f9c9d73ebb258725dc0525e3f17a8c43d54d01
= <drahtantnayak@dal.ca>
2019-02-05 19:31:00 -0400

    Remove previous code and add new functionality to complete task

--- LAB5TASK12.py
+++ LAB5TASK12.py
@@ -1,24 +1,18 @@
-import sys
-class IPAddress():
-    def __init__(self,ip_address,mask,cdir=24):
-        if '/' in ip_address:
-            self._address_val, self._cidr = ip.address.split('/') 
-            self._address = map(int, self._address_val.split('.'))
-        else:
-            self._address = map(int, ip_address.split('.'))
-            self._cidr = cdir
-        self.ip_address = ip_address
-        self.mask = mask
+from netaddr import *
+import pprint
+import socket
+class IP_Address():
+    def __init__(self,ipadd=socket.gethostbyname(socket.gethostname())):
+        self.ipadd = ipadd
 
-    def dec_to_binary(self):
-        return map(lambda x: bin(x)[2:].zfill(8), self._address)
- 
-    def __str__(self):
-        for i in self.dec_to_binary():
-            print(i)
-        return str(self.dec_to_binary())  + " " + str(self.mask)
+    def __repr__(self):
+        ip = IPNetwork(self.ipadd)
+        print("============== Network Information ==============")
+        print("Network:    " +  str(ip.network) +  "     " +  str(ip.network.bits()))
+        print("BroadCast:  " + str(ip.broadcast) + "   " + str(ip.broadcast.bits()))
+        print("First Host: " + str(ip[0]) + "     Last Host: " + str(ip[-1]))
+        print("Total Host: " + str(len(ip)))
 
-i = input("Enter IP Address to get network information:")
-m = input("Enter mask to get network information:")
-myobj = IPAddress(i,m)
-print(myobj)
+i = input("Enter IP address with netmask:")
+myobj = IP_Address(i)    
+myobj.__repr__()
